3ee46a8260cf0609f2b4e1df27f75a74d9f82667
Jason Washburn <jason@shyftsolutions.io>
2021-07-17 11:45:36 -0500

    added docker configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flask Weather App
+
+Experimenting with CI/CD using Flask, Docker, and GitHub Actions
--- app/app.py
+++ app/app.py
@@ -11,16 +11,19 @@ app = Flask(__name__)
 def index():
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}"
     city = 'Omaha'
-
-    r = requests.get(url.format(city, API_KEY)).json()
+    response = requests.get(url.format(city, API_KEY)).json()
 
     weather = {
         'city': city,
-        'temperature': round(r['main']['temp']),
-        'description': r['weather'][0]['description'].title(),
-        'icon': r['weather'][0]['icon']
+        'temperature': round(response['main']['temp']),
+        'description': response['weather'][0]['description'].title(),
+        'icon': response['weather'][0]['icon']
     }
 
     print(weather)
 
     return render_template('weather.html', weather=weather)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
