0c6f09fd2eff257b9eec166aec3fdbada6b35b41
Jason Washburn <jason@shyftsolutions.io>
2021-07-17 09:53:09 -0500

    initial capability, displays hardcoded weather info

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+import requests
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+API_KEY = os.getenv('API_KEY')
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}"
+    city = 'Omaha'
 
+    r = requests.get(url.format(city, API_KEY)).json()
 
-if __name__ == '__main__':
-    app.run()
+    weather = {
+        'city': city,
+        'temperature': round(r['main']['temp']),
+        'description': r['weather'][0]['description'].title(),
+        'icon': r['weather'][0]['icon']
+    }
+
+    print(weather)
+
+    return render_template('weather.html', weather=weather)
