f25a4f287fc7019709e978a786c31e71293afb00
Jason Washburn <jason@shyftsolutions.io>
2021-07-17 13:25:37 -0500

    added sqlite db and multi-city weather

--- app/app.py
+++ app/app.py
@@ -5,24 +5,34 @@ import os
 
 API_KEY = os.getenv('API_KEY')
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
+db = SQLAlchemy(app)
+
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
 
 
 @app.route("/")
 def index():
+    cities = City.query.all()
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}"
-    city = 'Omaha'
-    response = requests.get(url.format(city, API_KEY)).json()
 
-    weather = {
-        'city': city,
-        'temperature': round(response['main']['temp']),
-        'description': response['weather'][0]['description'].title(),
-        'icon': response['weather'][0]['icon']
-    }
+    weather_data = []
+    for city in cities:
+        response = requests.get(url.format(city.name, API_KEY)).json()
 
-    print(weather)
+        weather = {
+            'city': city.name,
+            'temperature': round(response['main']['temp']),
+            'description': response['weather'][0]['description'].title(),
+            'icon': response['weather'][0]['icon']
+        }
+        weather_data.append(weather)
 
-    return render_template('weather.html', weather=weather)
+    return render_template('weather.html', weather_data=weather_data)
 
 
 if __name__ == "__main__":
