d5288658f85304fb0f85e57cd96884708555740f
Jason Washburn <wburn@wburn.net>
2021-07-17 16:22:41 -0500

    added post capability

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -9,14 +9,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
 
-
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        new_city = request.form.get("city")
+        if new_city:
+            new_city_obj = City(name=new_city)
+            db.session.add(new_city_obj)
+            db.session.commit()
+
     cities = City.query.all()
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}"
 
